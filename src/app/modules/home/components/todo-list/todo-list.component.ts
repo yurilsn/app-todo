b2d84c23d419bbbc89681e4f2bef32189ef6e682
yurilsn <luizyuri57@gmail.com>
2024-01-27 00:32:57 -0300

    feat: implementando scss no componente todo-button-delete

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
+
+  taskList: Array<{task: string, checked: boolean}> = [];
+
+  ngOnInit(): void {
+      
+  }
 
 }
